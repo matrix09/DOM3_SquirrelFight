27f9f3e068b49644e130aeb6296c428abbaf3617
Erric <mateixzxf@icloud.com>
2017-11-18 11:26:22 +0800

    DOM3_1工程课件

---
+++ Assets/Scripts/BallCtrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCtrl : MonoBehaviour {
+
+    Rigidbody rb;
+    public float m_fDelayTime;
+    bool m_bTrigger = false;
+    float m_fStartTime;
+    public void OnStart(float delayTime)
+    {
+        m_bTrigger = true;
+        m_fDelayTime = delayTime;
+        m_fStartTime = Time.time;
+    }
+
+    void Update()
+    {
+        if(!m_bTrigger) return;
+
+        if (Time.time - m_fStartTime > m_fDelayTime)
+        {
+            m_bTrigger = false;
+            gameObject.AddComponent<Rigidbody>();
+        }
+
+    }
+
+    void OnCollisionEnter()
+    {
+        Destroy(gameObject);
+    }
+
+}

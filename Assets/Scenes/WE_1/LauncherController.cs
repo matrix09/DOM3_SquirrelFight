cd30e1f65b834e9ae418349218e1afd281ce9732
Erric <mateixzxf@icloud.com>
2017-11-24 20:47:35 +0800

    提交下落脚本

--- Assets/Scenes/WE_1/LauncherController.cs
+++ Assets/Scenes/WE_1/LauncherController.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class LauncherController : MonoBehaviour {
 
 
+    public BallLauncher[] m_cBallLaunchers;
+
+
     public void OnStart()
     {
-
+        for (int i = 0; i < m_cBallLaunchers.Length; i++)
+        {
+            m_cBallLaunchers[i].OnStart();
+        }
     }
 
     public void OnEnd()
     {
-
+        for (int i = 0; i < m_cBallLaunchers.Length; i++)
+        {
+            m_cBallLaunchers[i].OnEnd();
+        }
     }
 
 }

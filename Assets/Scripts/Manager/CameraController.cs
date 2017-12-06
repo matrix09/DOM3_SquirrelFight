bf44306a4da4c242898a8e529754e0c6f9cd25b6
Erric <mateixzxf@icloud.com>
2017-12-06 20:26:09 +0800

    添加角色位移

--- Assets/Scripts/Manager/CameraController.cs
+++ Assets/Scripts/Manager/CameraController.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+
+    public Transform m_tMajor;
+
+    public Vector3 offSet;
+
+    public Vector3 dir;
+
+    public float offSpeed;
+
+    public float dirSpeed;
+
 	// Update is called once per frame
 	void Update () {
-		
+
+        Vector3 pos = m_tMajor.position + offSet;
+
+        transform.position = Vector3.Lerp(transform.position, pos, offSpeed * Time.deltaTime);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(dir), dirSpeed * Time.deltaTime);
+
 	}
 }
--- Assets/Scripts/Manager/PlayerManager.cs
+++ Assets/Scripts/Manager/PlayerManager.cs
@@ -1,16 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Assets.Scripts.Stellar;
 public class PlayerManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+    public Stellar m_cStellar;  //获取曲线信息
+
+    float he = 0f;
+
+    float m_fT = 0f;
+    Vector3 pos;
+
+    Animator am;
+
+    void Start()
+    {
+        am = gameObject.GetComponent<Animator>();
+    }
+
 	void Update () {
-		
+
+        CharacterMove();    //角色运动
+
 	}
+
+    void CharacterMove()
+    {
+        he = Input.GetAxis("Horizontal");
+        if (he == 0f)
+        {
+            am.SetFloat("Speed", 0f);
+            return;
+        }
+         
+
+        if (m_fT >= 1f)
+            m_fT = 0f;
+
+        pos = m_cStellar.Interp(m_fT);
+
+        transform.forward = m_cStellar.GetDir(m_fT);
+
+        transform.position = Vector3.Lerp(transform.position, new Vector3(pos.x, transform.position.y, pos.z), 5 * Time.deltaTime);
+
+        m_fT += m_cStellar.GetDeltaT(m_fT) * Time.deltaTime * (he > 0f ? 1 : -1);
+
+
+        am.SetFloat("Speed", 1f);
+
+
+    }
+
 }
